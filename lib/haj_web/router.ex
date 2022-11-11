3c39378d281fb2644571134580b01e639cc9143d
Adrian Salamon <adr.salamon@gmail.com>
2022-11-11 08:46:26 +0100

    Made old merch admin page usable

--- lib/haj_web/router.ex
+++ lib/haj_web/router.ex
@@ -10,8 +10,8 @@ defmodule HajWeb.Router do
     plug :accepts, ["html"]
     plug :fetch_session
     plug :fetch_live_flash
-    plug :put_root_layout, {HajWeb.LayoutView, :root}
-    plug :put_layout, {HajWeb.LayoutView, :haj}
+    plug :put_root_layout, {HajWeb.Layouts, :root}
+    plug :put_layout, {HajWeb.Layouts, :haj}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
     plug :fetch_current_user
--- lib/haj_web/views/merch_admin_view.ex
+++ lib/haj_web/views/merch_admin_view.ex
@@ -1,5 +1,7 @@
-defmodule HajWeb.MerchAdminView do
+defmodule HajWeb.MerchAdminHTML do
   use HajWeb, :view
+
+  embed_templates("../templates/merch_admin/*")
 
   defp list_items(order) do
     Enum.map(order.merch_order_items, fn item -> "#{item.count} x #{item.merch_item.name}" end)
