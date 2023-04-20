db51d2d16f0e5931c1c9c168101f748d8e1f77ca
Adrian Salamon <adr.salamon@gmail.com>
2023-04-20 10:07:39 +0200

    added env variable for zfinger url, to enable caching

--- config/runtime.exs
+++ config/runtime.exs
@@ -39,6 +39,7 @@ if config_env() == :prod || config_env() == :staging do
   login_api_key = System.get_env("LOGIN_API_KEY") || raise "LOGIN_API_KEY is missing"
   login_host = System.get_env("LOGIN_HOST") || raise "LOGIN_HOST is missing"
   api_login_secret = System.get_env("API_LOGIN_SECRET") || raise "API_LOGIN_SECRET is missing"
+  zfinger_url = System.get_env("ZFINGER_URL") || "zfinger.datasektionen.se"
 
   config :haj,
     login_api_key: login_api_key,
