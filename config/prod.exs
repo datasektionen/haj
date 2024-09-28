2b4b40eed821126ff0aef4bcb410d4fbe4e0c963
Adrian Salamon <adr.salamon@gmail.com>
2024-09-28 13:56:06 +0200

    hotfix: don't force ssl

--- config/prod.exs
+++ config/prod.exs
@@ -49,4 +49,3 @@ config :logger, level: :info
 #       force_ssl: [hsts: true]
 #
 # Check `Plug.SSL` for all available options in `force_ssl`.
-config :haj, HajWeb.Endpoint, force_ssl: [hsts: true]
