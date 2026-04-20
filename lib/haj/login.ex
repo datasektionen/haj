a42b5e6fdddf03e41f388d9fe457fc4a22160513
Samuel <samuel.hornell@vaja.net>
2026-04-20 04:35:16 +0200

    added oidc client and stuff

--- lib/haj/login.ex
+++ lib/haj/login.ex
@@ -1,14 +1,9 @@
 defmodule Haj.Login do
   @moduledoc """
-  Module for interacting with `login` system. It is undocumented but works in the following way
+  Legacy helper module kept for backward compatibility.
   """
 
   def authorize_url() do
-    login_url = Application.get_env(:haj, :login_url)
-    hostname = Application.get_env(:haj, :hostname)
-    port = Application.get_env(:haj, :port)
-
-    callback = URI.encode("#{hostname}:#{port}/login/callback/?token=")
-    "#{login_url}/login?callback=#{callback}"
+    "/login"
   end
 end
